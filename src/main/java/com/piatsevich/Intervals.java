bccd2a31b0a9b2164d5f6dabb578779d9f24b935
Andrei Piatsevich <apiats2@gmail.com>
2023-03-12 18:59:16 +0300

    Initial project setup

---
+++ src/main/java/com/piatsevich/Intervals.java
@@ -0,0 +1,12 @@
+package com.piatsevich;
+
+public class Intervals {
+    public static String intervalConstruction(String[] args) {
+
+        return null;
+    }
+
+    public static String intervalIdentification(String[] args) {
+        return null;
+    }
+}
---
+++ src/main/java/com/piatsevich/Main.java
@@ -0,0 +1,7 @@
+package com.piatsevich;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
