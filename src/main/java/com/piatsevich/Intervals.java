a92dcdd31de1d42ea1089e995670c923481de019
Andrei Piatsevich <apiats2@gmail.com>
2023-03-14 14:59:49 +0300

    refactoring

--- src/main/java/com/piatsevich/Intervals.java
+++ src/main/java/com/piatsevich/Intervals.java
@@ -13,18 +13,13 @@ package com.piatsevich;
         P8 - Perfect Octave - 12 semitones, 8 degrees <br>
         */
 
-//        String[] notes = new String[]
-//                {"Cb", "C", "C#", "Db", "D", "D#", "Eb", "E", "E#", "Fb", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B", "B#"};
-
-// C C#/Db D D#/Eb E F F#/Gb G G#/Ab A A#/Bb B
-
+//        args = new String[] {"M2", "C"};
 
 import java.util.stream.IntStream;
 
 public class Intervals {
     public static String intervalConstruction(String[] args) {
 
-//        args = new String[]{"P5", "B"};
 
         // interval's index corresponds to number of semitones in interval
         String[] intervals = new String[]
@@ -34,32 +29,16 @@ public class Intervals {
         String[] notes = new String[]{"C/Dbb/B#", "C#/Db/B##", "D/C##/Ebb", "D#/Eb/Fbb",
                 "E/D##/Fb", "F/E#/Gbb", "F#/Gb/E##", "G/F##/Abb", "G#/Ab", "A/G##/Bbb", "A#/Bb/Cbb", "B/Cb/A##"};
 
-        String[] degrees = new String[]{"C", "D", "E", "F", "G", "A", "B"}; // Need left and right degrees
+        String[] degrees = new String[]{"C", "D", "E", "F", "G", "A", "B"};
 
         int semitonesCount = 0;
-        int degreeStartPointIndex = 0;
 
-        // Looking for degree left index
-        String degreeStartPoint = args[1].substring(0, 1);
-//        for (int i = 0; i < degrees.length; i++) {
-//            if (degrees[i].equals(degreeStartPoint)) {
-//                degreeStartPointIndex = i;
-//            }
-//        }
-//
-//        // Looking for degree right index. If degreeEndPointIndex out of boundary, move it to the beginning
         int degreeStepsCount = Integer.parseInt(args[0].substring(1));
-//        int degreeEndPointIndex = degreeStartPointIndex + degreeStepsCount -1;
-//
-//        if (degreeEndPointIndex > 6) {
-//            degreeEndPointIndex = degreeEndPointIndex - 8;
-//        }
+
         int rightDegreeIndex = 0;
         int rightDegreeIndexDsc = 0;
 
-        //        args = new String[]{"P5", "B"};
-
-
+        // Looking for degree right index. If degreeEndPointIndex out of boundary, move it to the beginning
         for (int i = 0; i < degrees.length; i++) {
             if (degrees[i].equals(args[1].substring(0,1))) {
                 rightDegreeIndex = (i + degreeStepsCount) % degrees.length - 1;
@@ -68,29 +47,14 @@ public class Intervals {
             }
         }
 
-        // semitones count in intervals
-//        for (int i = 0; i < intervals.length; i++) {
-//            if (intervals[i].equals(args[0])) {
-//                semitonesCount = i;
-//            }
-//        }
-
-        // semitones count in intervals
+        // semitones count in intervals(return index of interval=NofSemitones)
         semitonesCount = IntStream
                 .range(0, intervals.length)
                 .filter(i -> intervals[i].equals(args[0]))
                 .findFirst().orElse(-1);
 
-        //        args = new String[]{"P5", "B"};
-
-        // all possible notes combinations
-        String[] notees = new String[]{"C/Dbb/B#", "C#/Db/B##", "D/C##/Ebb", "D#/Eb/Fbb",
-                "E/D##/Fb", "F/E#/Gbb", "F#/Gb/E##", "G/F##/Abb", "G#/Ab", "A/G##/Bbb", "A#/Bb/Cbb", "B/Cb/A##"};
-
 
         int intervalLeftIndex = 0;
-        int intervalRightIndex = 0;
-        int intervalRightIndexDsc = 0;
 
         // left interval index lookup in notes
         for (int i = 0; i < notes.length; i++) {
@@ -103,54 +67,20 @@ public class Intervals {
             }
         }
 
-        for (int i = 0; i < notes.length; i++) {
-            if (degrees[i].equals("E")) {
-                break;
-            }
-        }
-
-        int rightNoteIndex = (intervalLeftIndex + semitonesCount) % notes.length - 1;
-        int rightNoteIndexDsc = ((intervalLeftIndex - semitonesCount + 1) + notees.length) % notees.length;
-
         // right interval index lookup in notes
-        intervalRightIndex = intervalLeftIndex + semitonesCount;
-        if (intervalRightIndex > 11) {
-            intervalRightIndex = intervalRightIndex - 12;
-        }
+        int rightNoteIndex = (intervalLeftIndex + semitonesCount) % notes.length;
+        int rightNoteIndexDsc = ((intervalLeftIndex - semitonesCount) + notes.length) % notes.length;
 
-        //{"C", "D", "E", "F", "G", "A", "B"};
-        String[] nqotes = new String[]{"C/Dbb/B#", "C#/Db/B##", "D/C##/Ebb", "D#/Eb/Fbb",
-                "E/D##/Fb", "F/E#/Gbb", "F#/Gb/E##", "G/F##/Abb", "G#/Ab", "A/G##/Bbb", "A#/Bb/Cbb", "B/Cb/A##"};
-
-        String[] rightNote = notes[intervalRightIndex].split("/");
+        //Splitting notes
+        String[] rightNote = notes[rightNoteIndex].split("/");
         String result = null;
         for (String splitNote : rightNote) {
             if (splitNote.substring(0, 1).equals(degrees[rightDegreeIndex])) {
-                String test = splitNote.substring(0, 1);
                 result = splitNote;
             }
-
         }
 
-
-        System.out.println(result);
-
-
-        /*
-        input:
-            notes: Cb C C# Db D D# Eb E E# Fb F F# Gb G G# Ab A A# Bb B B#
-            notes: "Cb", "C", "C#", "Db", "D", "D#", "Eb", "E", "E#", "Fb", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B", "B#"
-            intervals: m2 M2 m3 M3 P4 P5 m6 M6 m7 M7 P8
-
-        return:
-        Cbb Cb C C# C## Dbb Db D D# D## Ebb Eb E E# E## Fbb Fb F F# F## Gbb Gb G G# G## Abb Ab A A# A## Bbb Bb B B# B##
-
-        ['M3', 'A', 'asc'] - build an ascending M3 interval starting from A <br>
-        ['m7, 'Fb', 'dsc'] - build an descending m7 interval starting from Fb <br>
-        ['P4', 'E#'] - build an ascending P4 interval starting from E# <br>
-         */
-
-        return null;
+        return result;
     }
 
     public static String intervalIdentification(String[] args) {
--- src/main/java/com/piatsevich/Main.java
+++ src/main/java/com/piatsevich/Main.java
@@ -1,48 +1,19 @@
 package com.piatsevich;
-
-
-import java.util.stream.IntStream;
-
+git
 public class Main {
     public static void main(String[] args) {
 
-        String[] input = new String[]{"M7", "G"};
-        Intervals.intervalConstruction(input);
-
-
-        // looking for right degree index for accenting/descending orders
-        int degreeStepsCount = Integer.parseInt(input[0].substring(1));
-        int rightDegreeIndex = 0;
-        int rightDegreeIndexDsc = 0;
-        String[] degrees = new String[]{"C", "D", "E", "F", "G", "A", "B"};
-
-        for (int i = 0; i < degrees.length; i++) {
-            if (degrees[i].equals("E")) {
-                rightDegreeIndex = (i + degreeStepsCount) % degrees.length - 1;
-                rightDegreeIndexDsc = ((i - degreeStepsCount + 1) + degrees.length) % degrees.length;
-                break;
-            }
-        }
-        int semitonesCount = 0;
-
-
-        String[] intervals = new String[]
-                {"", "m2", "M2", "m3", "M3", "P4", "", "P5", "m6", "M6", "m7", "M7", "P8"};
-
-        int semCount = IntStream
-                .range(0, intervals.length)
-                .filter(i -> intervals[i].equals("m2"))
-                .findFirst().orElse(-1);
-
-        System.out.println(semCount);
-
-
-        // semitones count in intervals
-        for (int i = 0; i < intervals.length; i++) {
-            if (intervals[i].equals(input[0])) {
-                semitonesCount = i;
-            }
-        }
+        String[] input = new String[]{"M2", "C"};
+        String[] input1 = new String[]{"P5", "B"};
+        String[] input2 = new String[]{"m2", "Fb"};
+        String[] input3 = new String[]{"m2", "D#"};
+        String[] input4 = new String[]{"M7", "G"};
+        System.out.println(Intervals.intervalConstruction(input));
+        Intervals.intervalConstruction(input1);
+        Intervals.intervalConstruction(input2);
+        Intervals.intervalConstruction(input3);
+        Intervals.intervalConstruction(input4);
+// D F# Gbb E F#
 
     }
 }
