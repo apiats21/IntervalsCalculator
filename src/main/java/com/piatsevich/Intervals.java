d8f150364d98c8fd5c82883271e41657aadbd8af
Andrei Piatsevich <apiats2@gmail.com>
2023-03-15 16:19:49 +0300

    Refactoring

--- src/main/java/com/piatsevich/Intervals.java
+++ src/main/java/com/piatsevich/Intervals.java
@@ -4,21 +4,20 @@ import java.util.stream.IntStream;
 
 public class Intervals {
     // interval's index corresponds to number of semitones in interval
-    private  static final String[] intervals = new String[]
+    private static final String[] intervalList = new String[]
             {"", "m2", "M2", "m3", "M3", "P4", "", "P5", "m6", "M6", "m7", "M7", "P8"};
 
     // all possible notes combinations
-    private  static final String[] notes = new String[]{"C/Dbb/B#", "C#/Db/B##", "D/C##/Ebb", "D#/Eb/Fbb",
+    private static final String[] completeNoteList = new String[]{"C/Dbb/B#", "C#/Db/B##", "D/C##/Ebb", "D#/Eb/Fbb",
             "E/D##/Fb", "F/E#/Gbb", "F#/Gb/E##", "G/F##/Abb", "G#/Ab", "A/G##/Bbb", "A#/Bb/Cbb", "B/Cb/A##"};
 
-    private  static final String[] degrees = new String[]{"C", "D", "E", "F", "G", "A", "B"};
+    private static final String[] basicNoteList = new String[]{"C", "D", "E", "F", "G", "A", "B"};
 
 
     public static String intervalConstruction(String[] args) {
 
         //  - If there are more or fewer elements in the input array, an exception should be thrown: "Illegal number of elements in input array"
 
-        int semitonesCount = 0;
         int degreeStepsCount = Integer.parseInt(args[0].substring(1));
 
         int endDegreeIndex = 0;
@@ -26,29 +25,26 @@ public class Intervals {
 
         boolean descending = args.length == 3 && args[2].equals("dsc");
 
-        // Looking for degree end index.
-        for (int i = 0; i < degrees.length; i++) {
-            if (degrees[i].equals(args[1].substring(0, 1))) {
-                endDegreeIndex = (i + degreeStepsCount) % degrees.length - 1;
-                endDegreeIndexDsc = ((i - degreeStepsCount + 1) + degrees.length) % degrees.length;
+        // Looking for basicNoteList end index.
+        for (int i = 0; i < basicNoteList.length; i++) {
+            if (basicNoteList[i].equals(args[1].substring(0, 1))) {
+                endDegreeIndex = (i + degreeStepsCount) % basicNoteList.length - 1;
+                endDegreeIndexDsc = ((i - degreeStepsCount + 1) + basicNoteList.length) % basicNoteList.length;
                 break;
             }
         }
 
         // semitones count in intervals(return index of interval=SemitonesCount)
-        semitonesCount = IntStream
-                .range(0, intervals.length)
-                .filter(i -> intervals[i].equals(args[0]))
-                .findFirst().orElse(-1);
 
+        int semitonesCount = semitoneCounter(args);
 
         int intervalStartIndex = 0;
 
         // left note index lookup
-        for (int i = 0; i < notes.length; i++) {
-            String[] splittedNote = notes[i].split("/");
+        for (int i = 0; i < completeNoteList.length; i++) {
+            String[] splittedNote = completeNoteList[i].split("/");
             for (String note : splittedNote) {
-                if (note.equalsIgnoreCase(args[1])) {
+                if (note.equals(args[1])) {
                     intervalStartIndex = i;
                     break;
                 }
@@ -56,8 +52,8 @@ public class Intervals {
         }
 
         // right interval index lookup in notes
-        int rightNoteIndex = (intervalStartIndex + semitonesCount) % notes.length;
-        int rightNoteIndexDsc = ((intervalStartIndex - semitonesCount) + notes.length) % notes.length;
+        int rightNoteIndex = (intervalStartIndex + semitonesCount) % completeNoteList.length;
+        int rightNoteIndexDsc = ((intervalStartIndex - semitonesCount) + completeNoteList.length) % completeNoteList.length;
 
         if (descending) {
             rightNoteIndex = rightNoteIndexDsc;
@@ -65,10 +61,10 @@ public class Intervals {
         }
 
         //Splitting notes
-        String[] rightNote = notes[rightNoteIndex].split("/");
+        String[] rightNote = completeNoteList[rightNoteIndex].split("/");
         String result = null;
         for (String splitNote : rightNote) {
-            if (splitNote.substring(0, 1).equals(degrees[endDegreeIndex])) {
+            if (splitNote.substring(0, 1).equals(basicNoteList[endDegreeIndex])) {
                 result = splitNote;
             }
         }
@@ -81,29 +77,36 @@ public class Intervals {
 
         boolean descending = args.length == 3 && args[2].equals("dsc");
 
-        int intervalLeftIndex = 0;
-        int intervalRightIndex = 0;
+        int intervalStartIndex = 0;
+        int intervalEndIndex = 0;
 
         // left & right note index lookup
-        for (int i = 0; i < notes.length; i++) {
-            String[] splittedNote = notes[i].split("/");
+        for (int i = 0; i < completeNoteList.length; i++) {
+            String[] splittedNote = completeNoteList[i].split("/");
             for (String note : splittedNote) {
-                if (note.equals(args[0])) {
-                    intervalLeftIndex = i;
-                }
                 if (note.equals(args[1])) {
-                    intervalRightIndex = i;
+                    intervalEndIndex = i;
+                }
+                if (note.equals(args[0])) {
+                    intervalStartIndex = i;
                 }
             }
         }
 
-        int semitonesCount = ((intervalRightIndex - intervalLeftIndex) + notes.length) % notes.length;
-        int semitonesCountDes = ((intervalLeftIndex - intervalRightIndex) + notes.length) % notes.length;
+        int semitonesCount = ((intervalEndIndex - intervalStartIndex) + completeNoteList.length) % completeNoteList.length;
+        int semitonesCountDes = ((intervalStartIndex - intervalEndIndex) + completeNoteList.length) % completeNoteList.length;
 
         if (descending) {
-            return intervals[semitonesCountDes];
+            return intervalList[semitonesCountDes];
         }
 
-        return intervals[semitonesCount];
+        return intervalList[semitonesCount];
+    }
+
+    private static int semitoneCounter(String[] args) {
+        return IntStream
+                .range(0, intervalList.length)
+                .filter(i -> intervalList[i].equals(args[0]))
+                .findFirst().orElse(-1);
     }
 }
